ffd2eb4590fcb4cb7fcff33bab97393e91bd7f6d
CoreManish <72579593+CoreManish@users.noreply.github.com>
2022-07-03 03:51:27 +0530

    Create 3.scatter-in-2d-when-data-in-list.py

---
+++ MLT/matplotlib/3.scatter-in-2d-when-data-in-list.py
@@ -0,0 +1,9 @@
+# Draw scatter plot when x and y is in python list
+
+from matplotlib import pyplot as plt
+
+x=[1,2,3,4,5]
+y=[2,4,5,1,2]
+
+plt.scatter(x,y,color='red') #default color=blue
+plt.show()
